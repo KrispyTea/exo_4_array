855fea0ccc2f374157202738a8669a1eb63c2a80
KrispyTea <80962605+KrispyTea@users.noreply.github.com>
2021-04-16 15:06:16 +0200

    over

---
+++ main.js
@@ -0,0 +1,8 @@
+let array = ["coding 11","coding 12","coding 13"]
+console.log(array);
+array.push("Alain")
+console.log(array)
+array.splice(2,1,"Chocolat")
+console.log(array)
+array.splice(1,1)
+console.log(array);
